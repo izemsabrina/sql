c8244fa675df7b9ddec5aa4f6c4d3e38193d04bf
sabrina <sabrinizem@gmail.com>
2023-03-31 11:51:48 +0200

    sql1

---
+++ jour1/05-table.sql
@@ -0,0 +1,17 @@
+--commentaire en sql--
+-- creation d'une table
+-- etudiant 
+-- 2colonnes
+-- prenom tesxte avec max de 255caracteres
+-- nom teste avec maxim de 255 carecteres
+CREATE TABLE IF NOT EXISTS etudiant (
+prenom VARCHAR(255),
+nom VARCHAR(255)
+);
+-- une fois elle est ecrite on l'execule
+
+CREATE TABLE IF NOT EXISTS exo1(
+adresee VARCHAR(255),
+ville VARCHAR(255),
+nom VARCHAR(255)
+);
---
+++ jour1/06-type.sql
@@ -0,0 +1,22 @@
+-- nbr de caractere
+VARCHAR 
+TEXT 
+MEDIUMTEXT
+LONGTEXT
+BLOB
+-- chiffres 
+-- entier
+TUNYINT
+SMALLINT
+MEDIUMINT
+INT
+BIGINT
+-- chiffre a virgule
+BBECIMAL
+FLOAT
+-- chiffre spéciaux
+BOOLEAN
+-- DATES
+TIME 
+DATA
+DATATIME
